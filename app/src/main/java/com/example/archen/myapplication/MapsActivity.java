68c3391a7a3b93e475f0be59733ee7ae2e16f40b
anrchen <ky_chen93@hotmail.com>
2017-01-21 21:47:41 -0500

    Map finalized

--- app/src/main/java/com/example/archen/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/archen/myapplication/MapsActivity.java
@@ -2,6 +2,7 @@ package com.example.archen.myapplication;
 
 import android.*;
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Geocoder;
 import android.os.Build;
@@ -10,6 +11,7 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -23,13 +25,24 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import java.io.IOException;
 import java.util.List;
 import android.location.Address;
+import android.widget.Toast;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private static final LatLng PERTH = new LatLng(-31.952854, 115.857342);
+    private static final LatLng MONTREAL = new LatLng(21.2112, 123.12321);
     private GoogleMap mMap;
     private Marker mPerth;
+    private Marker mMontreal;
     private final static int my_location=101;
+
+
+
+    DatabaseHelper myDB;
+    Button btnAdd,btnView;
+    EditText editText;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +52,42 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+
+
+
+//        setContentView(R.layout.activity_main);
+//        editText = (EditText) findViewById(R.id.editText);
+//        setContentView(R.layout.activity_maps);
+        editText = (EditText) findViewById(R.id.TFaddress);
+//        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+//        btnView = (Button) findViewById(R.id.btnView);
+        btnView = (Button) findViewById(R.id.btnView);
+        myDB = new DatabaseHelper(this);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newEntry = editText.getText().toString();
+                if(editText.length()!= 0){
+                    AddData(newEntry);
+                    editText.setText("");
+                }else{
+                    Toast.makeText(MapsActivity.this, "You must put something in the text field!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        btnView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MapsActivity.this, ViewListContents.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 
@@ -66,31 +115,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
-//    public void onSearch(View view){
-//        EditText location_tf = (EditText)findViewById(R.id.TFaddress);
-//        String location = location_tf.getText().toString();
-//        List<Address> addressList=null;
-//        if(!location.equals("")){
-//            Geocoder geocoder = new Geocoder(this);
-//            try {
-//                addressList=geocoder.getFromLocationName(location, 1);
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//
-//            Address address = addressList.get(0);
-//            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
-//
-//            mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
-//            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-//        }
-//    }
+    public void onSearch(View view){
+        EditText location_tf = (EditText)findViewById(R.id.TFaddress);
+        String location = location_tf.getText().toString();
+        List<Address> addressList=null;
+        if(!location.equals("")){
+            Geocoder geocoder = new Geocoder(this);
+            try {
+                addressList=geocoder.getFromLocationName(location, 1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Address address = addressList.get(0);
+            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+
+            mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        }
+    }
 
     private void setUpMap(){
         mPerth = mMap.addMarker(new MarkerOptions()
                 .position(PERTH)
                 .title("Perth"));
         mPerth.setTag(0);
+
+        mMontreal = mMap.addMarker(new MarkerOptions()
+                .position(MONTREAL)
+                .title("Ice cream")
+        );
         mMap.addMarker(new MarkerOptions().position(new LatLng(0,0)).title("Marker"));
 //        &&ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED
         if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
@@ -104,4 +158,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
+    public void AddData(String newEntry) {
+
+        boolean insertData = myDB.addData(newEntry);
+
+        if(insertData==true){
+            Toast.makeText(this, "Data Successfully Inserted!", Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(this, "Something went wrong :(.", Toast.LENGTH_LONG).show();
+        }
+    }
 }
+
+
+
